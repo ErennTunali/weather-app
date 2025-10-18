ae9cc047bfa79684d0273a84cdfee56ed3c1cec2
ErennTunali <156017553+ErennTunali@users.noreply.github.com>
2025-10-18 10:03:22 +0300

    added the api call

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import './styles.css';
+const KEY = 'PS2CXE5UFEY6ZXS27ERF5CVZL';
+// let city = 'istanbul';
+
+async function getWeatherInfo() {
+  let response = await fetch(
+    'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/istanbul?key=' +
+      KEY
+  );
+  let data = await response.json();
+  console.log(data);
+}
+getWeatherInfo();
