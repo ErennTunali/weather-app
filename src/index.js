b67cfe2fef7003ecbe8afc55feb91faade441a28
ErennTunali <156017553+ErennTunali@users.noreply.github.com>
2025-10-22 00:47:27 +0300

    got the input logic working successfully

--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
 import './styles.css';
 const KEY = 'PS2CXE5UFEY6ZXS27ERF5CVZL';
+
+let inputSection = document.querySelector('input');
+let infoSection = document.querySelector('.info-section');
+let submitButton = document.querySelector('.submit-button');
 // let city = 'istanbul';
 
-async function getWeatherInfo() {
+async function getWeatherInfo(city) {
   let response = await fetch(
-    'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/istanbul?key=' +
+    'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/' +
+      city +
+      '?key=' +
       KEY
   );
   let data = await response.json();
-  console.log(data);
+  return data;
 }
-getWeatherInfo();
+getWeatherInfo('istanbul');
+
+submitButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  let city = inputSection.value;
+  inputSection.value = '';
+  updateInfo(city);
+});
+
+async function updateInfo(city) {
+  let weatherData = await getWeatherInfo(city);
+  let weatherDesc = weatherData.description;
+  infoSection.innerText = weatherDesc;
+}
